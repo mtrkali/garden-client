391c5caee4f6b850da5c21bc51db4d41e8c0567e
mtrkali <tarakfeni95@gmail.com>
2025-08-24 12:46:04 +0600

    fix navbar width issue

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -5,7 +5,7 @@ import Footer from '../Pages/Footer';
 
 const Root = () => {
     return (
-        <div>
+        <div className='w-full lg:w-11/12 mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
